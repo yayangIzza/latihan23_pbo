7459e8e235d0c444191d48d1086e16e065493b48
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:46:01 +0700

    Add files via upload

---
+++ nilaibesarkecil.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+ echo '=====Program Nilai Terbesar dan Terkecil Nilai Mahasiswa=====<br>';
+echo "Masukkan nama petugas : kurniawan<br>";
+      echo "Masukkan banyaknya nilai mahasiswa : 4<br>";
+      echo "Masukkan nilai mahasiswa ke-1 = 32<br>";
+      echo "Masukkan nilai mahasiswa ke-2 = 0<br>";
+      echo "Masukkan nilai mahasiswa ke-3 = 77<br>";
+      echo "Masukkan nilai mahasiswa ke-4 = 15<br>";
+
+echo '=====Hasil Nilai Mahasiswa=====<br>';
+      echo "masukkan Nilai mahasiswa ke-1 = 32<br>";
+      echo "Masukkan nilai mahasiswa ke-2 = 0<br>";
+      echo "Masukkan nilai mahasiswa ke-3 = 77<br>";
+      echo "Masukkan nilai mahasiswa ke-4 = 15<br>";
+
+    
+      echo "nilai terbesar adalah : 77<br>";
+      echo "nilai terkecil adalah : 0<br>";
+
+
+
+?>
